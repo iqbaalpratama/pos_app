db77c721b3051775e776896dce5aeb175de093d2
iqbaalpratama <iqbaalpratama23@gmail.com>
2021-05-08 23:26:26 +0700

    add progress bar

--- SplashScreen.Designer.cs
+++ SplashScreen.Designer.cs
@@ -29,10 +29,13 @@ namespace pos_wpf
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashScreen));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -61,12 +64,23 @@ namespace pos_wpf
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(244, 143);
+            this.pictureBox1.Location = new System.Drawing.Point(243, 130);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(125, 94);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(77, 242);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(479, 11);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // SplashScreen
             // 
@@ -74,11 +88,13 @@ namespace pos_wpf
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Maroon;
             this.ClientSize = new System.Drawing.Size(639, 280);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "SplashScreen";
             this.Text = "SplashScreen";
+            this.Load += new System.EventHandler(this.SplashScreen_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -90,5 +106,7 @@ namespace pos_wpf
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- SplashScreen.cs
+++ SplashScreen.cs
@@ -14,5 +14,25 @@ namespace pos_wpf
         {
             InitializeComponent();
         }
+        int startpoint = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            startpoint += 1;
+            progressBar1.Value = startpoint;
+            if(progressBar1.Value==100)
+            {
+                progressBar1.Value = 0;
+                timer1.Stop();
+                LoginForm log = new LoginForm();
+                this.Hide();
+                log.Show();
+            }
+        }
+
+        private void SplashScreen_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
     }
 }
